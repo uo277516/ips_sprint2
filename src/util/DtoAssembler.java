281369903114bc4a9d34953cd07c8dc2b032fa6e
joser <joser@LAPTOP-59RJKCU5>
2021-11-09 18:29:46 +0100

    historia 15320 casi acabada falta unirla al crear competicion

--- src/util/DtoAssembler.java
+++ src/util/DtoAssembler.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.List;
 
 import logica.AtletaDto;
+import logica.CategoriaDto;
 import logica.CompeticionDto;
 import logica.InscripcionDto;
 
@@ -85,6 +86,32 @@ public class DtoAssembler {
 		a.setDorsales_vip(Integer.parseInt(rs.getString("dorsales_vip")));
 		return a;
 	}
+	
+	public static List<CategoriaDto> toCotegoriaDtoList (ResultSet rs)
+	{
+		List<CategoriaDto> lista = new ArrayList<CategoriaDto>();
+		try {
+			while(rs.next())
+			{
+				lista.add(cogerDatosCategoria(rs));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lista;
+	}
+
+	private static CategoriaDto cogerDatosCategoria(ResultSet rs) throws SQLException {
+		CategoriaDto a = new CategoriaDto();
+		
+		a.setId((rs.getString("id")));
+		a.setNombre(rs.getString("nombre"));
+		a.setEdad_min(Integer.parseInt(rs.getString("edad_min")));
+		a.setEdad_max(Integer.parseInt(rs.getString("edad_max")));
+		a.setSexo(rs.getString("sexo"));
+		return a;
+	}
 
 	public static List<InscripcionDto> toInscripcionDtoList(ResultSet rs) {
 		List<InscripcionDto> lista = new ArrayList<InscripcionDto>();
@@ -98,6 +125,12 @@ public class DtoAssembler {
 			e.printStackTrace();
 		}
 		return lista;
+	}
+	
+	public static List<Integer> toListInt(ResultSet rs) throws SQLException {
+		List<Integer> list = new ArrayList<>();
+		list.add(rs.getInt("maximo"));
+		return list;
 	}
 
 	
@@ -188,4 +221,6 @@ public class DtoAssembler {
 		
 
 	}
+
+	
 }
